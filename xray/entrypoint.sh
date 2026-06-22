8e16c395a7961b433fd54c1374c643fc4530839a
k1racle <122564272+k1racle@users.noreply.github.com>
2026-06-22 16:10:19 +0300

    fix: use sed instead of envsubst in xray entrypoint

--- xray/entrypoint.sh
+++ xray/entrypoint.sh
@@ -6,5 +6,9 @@ if [ -z "$XRAY_ADDRESS" ] || [ -z "$XRAY_PORT" ] || [ -z "$XRAY_ID" ]; then
     exit 1
 fi
 
-envsubst < /etc/xray/client.json.template > /etc/xray/config.json
+sed -e "s|\\${XRAY_ADDRESS}|$XRAY_ADDRESS|g" \
+    -e "s|\\${XRAY_PORT}|$XRAY_PORT|g" \
+    -e "s|\\${XRAY_ID}|$XRAY_ID|g" \
+    /etc/xray/client.json.template > /etc/xray/config.json
+
 exec /usr/bin/xray -config /etc/xray/config.json
